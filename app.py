aa800041bc9e9075b0807c0d7e73f3c91f869d0f
GRVBD <61073862+gdharanidharan@users.noreply.github.com>
2022-07-03 03:12:59 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,70 @@
+import pandas as pd
+import numpy as np
+import pickle
+import streamlit as st
+from PIL import Image
+
+pickle_in = open('classifier.pkl', 'rb')
+classifer = pickle.load(pickle_in)
+
+
+def welcome():
+    return 'Welcome all'
+
+
+def predict_note_authentication(variance,skewness,curtosis,entropy):
+
+  """
+  Let's Authenticate the Banks Note 
+  This is using docstrings for specifications.
+  ---
+  parameters:  
+    - name: variance
+      in: query
+      type: number
+      required: true
+    - name: skewness
+      in: query
+      type: number
+      required: true
+    - name: curtosis
+      in: query
+      type: number
+      required: true
+    - name: entropy
+      in: query
+      type: number
+      required: true
+  responses:
+      200:
+          description: The output values
+  """
+
+  prediction = classifer.predict([[variance, skewness, curtosis, entropy]])
+  return prediction
+
+
+def main():
+  st.title("Bank Authenticator")
+
+  html_temp = """
+  <div style="background-color:tomato;padding:10px">
+  <h2 style="color:white;text-align:center;">Streamlit Bank Authenticator ML App </h2>
+  </div>
+  """
+  st.markdown(html_temp,unsafe_allow_html=True)
+  variance = st.text_input("Variance","Type Here")
+  skewness = st.text_input("skewness","Type Here")
+  curtosis = st.text_input("curtosis","Type Here")
+  entropy = st.text_input("entropy","Type Here")
+
+  result = ""
+  if st.button("Predict"):
+      result = predict_note_authentication(variance,skewness,curtosis,entropy)
+  st.success('The output is {}'.format(result))
+  if st.button("About"):
+      st.text("Lets Learn")
+      st.text("Built with Streamlit")
+
+if __name__ == '__main__':
+    main()
